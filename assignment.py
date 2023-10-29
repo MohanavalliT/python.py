36c812b6fc2ca8eec7f04cdc3a3403ab90476ebd
MOHANAVALLI T <118752312+MohanavalliT@users.noreply.github.com>
2023-10-29 17:50:36 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,9 @@
+x=10
+print(x)
+print("The x+=20 is =",x)
+print("The x-=5 is +",x)
+print("The x/=2 is =",x)
+print("The x*=3 is =",x)
+print("The x%=4 is =",x)
+print("The x***3 is =",x)
+print("The x//3 is =",x)
